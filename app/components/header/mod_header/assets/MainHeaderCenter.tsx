bb156f31b8a849edc8f57f81e10637f9c7f30e67
n2hq <n2hq.co@gmail.com>
2025-11-14 13:33:42 +0100

    Test: update commit

--- app/components/header/mod_header/assets/MainHeaderCenter.tsx
+++ app/components/header/mod_header/assets/MainHeaderCenter.tsx
@@ -18,7 +18,7 @@ const MainHeaderCenter = ({ showMenu, closeMenu }: MainHeaderCenterProps) => {
             {
                 mainHeaderCenterMenu.map((menu, index: number) => {
                     return (
-                        <div key={index} className={`main-header-menu-item ${location.pathname === menu.url && ' bg-blue-500 text-white'}`}>
+                        <div key={index} className={`main-header-menu-item border border-gray-50/0 ${location.pathname === menu.url && ' bg-gray-200 text-black'}`}>
                             <Link to={`${menu.url}`}>
                                 <div className={`main-header-menu-item-link `}>
                                     {/* <div className='main-header-menu-item-link-icon'>
@@ -47,7 +47,7 @@ export default MainHeaderCenter
 export const More = ({ showMenu, closeMenu }: MainHeaderCenterProps) => {
     return (
         <div
-            className={`bg-blue-300/40 flex place-items-center w-7 place-content-center rounded-full h-7 font-bold cursor-pointer`}
+            className={`bg-gray-300/40 flex place-items-center w-7 place-content-center rounded-full h-7 font-bold cursor-pointer`}
             onClick={() => showMenu()}>
             <BsThreeDotsVertical />
         </div>

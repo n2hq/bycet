12b0818dcd17d0b9c476471d409e3efc8662c8a6
n2hq <n2hq.co@gmail.com>
2025-11-20 08:35:23 +0100

    Test: update commit

--- build/server/index.js
+++ build/server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(
   createRequestHandler({
-    build: () => import("./assets/server-build-Csy-hCVE.js"),
+    build: () => import("./assets/server-build-Bgz1Ens9.js"),
     getLoadContext() {
       return {
         VALUE_FROM_EXPRESS: "Hello from Express"
